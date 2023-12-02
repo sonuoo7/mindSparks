7ec70871ec6aee1780d4fad07d44e8e249835e8f
Subh Pant <subhash.pant@rensoltechnologies.com>
2023-12-02 20:29:33 +0530

    change QNA Web To QNA_App

--- QNA_App/Controllers/DashBoardController.cs
+++ QNA_App/Controllers/DashBoardController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace QNAWeb.Controllers
+namespace QNA_App.Controllers
 {
     public class DashBoardController : Controller
     {
--- QNA_App/Controllers/HomeController.cs
+++ QNA_App/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace QNAWeb.Controllers {
+namespace QNA_App.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
             return View();
